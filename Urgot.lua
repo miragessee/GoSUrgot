d5ce295432627ae7c6661733c90a9b223a977b75
miragessee <miragessee@hotmail.com>
2018-10-02 03:19:17 +0300

    added AutoUpdate
    added AutoUpdate

--- Urgot.lua
+++ Urgot.lua
@@ -1,5 +1,53 @@
 if myHero.charName ~= "Urgot" then return end
 
+-- [ update ]
+do
+    
+    local Version = 1
+    
+    local Files = {
+        Lua = {
+            Path = SCRIPT_PATH,
+            Name = "Urgot.lua",
+            Url = "https://raw.githubusercontent.com/miragessee/GoSUrgot/master/Urgot.lua"
+        },
+        Version = {
+            Path = SCRIPT_PATH,
+            Name = "miragesurgot.version",
+            Url = "https://raw.githubusercontent.com/miragessee/GoSUrgot/master/miragesurgot.version"
+        }
+    }
+    
+    local function AutoUpdate()
+        
+        local function DownloadFile(url, path, fileName)
+            DownloadFileAsync(url, path .. fileName, function() end)
+            while not FileExist(path .. fileName) do end
+        end
+        
+        local function ReadFile(path, fileName)
+            local file = io.open(path .. fileName, "r")
+            local result = file:read()
+            file:close()
+            return result
+        end
+        
+        DownloadFile(Files.Version.Url, Files.Version.Path, Files.Version.Name)
+        
+        local NewVersion = tonumber(ReadFile(Files.Version.Path, Files.Version.Name))
+        if NewVersion > Version then
+            DownloadFile(Files.Lua.Url, Files.Lua.Path, Files.Lua.Name)
+            print(Files.Version.Name .. ": Updated to " .. tostring(NewVersion) .. ". Please Reload with 2x F6")
+        else
+            print(Files.Version.Name .. ": No Updates Found")
+        end
+    
+    end
+    
+    AutoUpdate()
+
+end
+
 local _atan = math.atan2
 local _min = math.min
 local _abs = math.abs
